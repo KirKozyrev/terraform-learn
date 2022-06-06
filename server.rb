6aaf6bca6a3b44b3e3cd7622dcb073c40cbbaeba
Кирилл Козырев <kozyrev.k1995@gmail.com>
2022-06-06 23:31:21 +0300

    fix tabs

--- server.rb
+++ server.rb
@@ -1,9 +1,9 @@
 configure do
-    set :server, :puma
-    set :port, ENV['PORT']
-    set :bind, '0.0.0.0'
-  end
-  
-  get '/ping' do
-    'pong'
-  end
+  set :server, :puma
+  set :port, ENV['PORT']
+  set :bind, '0.0.0.0'
+end
+
+get '/ping' do
+  'pong'
+end
